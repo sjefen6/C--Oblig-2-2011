1393256287504f3265e5d0881ae2e59331a89608
Vegard Lang&aring <sjefen6@gmail.com>
2011-12-03 19:34:39 +0100

    Fikser plasseringen på husene, fixer random etasjer

--- CityDefender/House.cs
+++ CityDefender/House.cs
@@ -10,46 +10,46 @@ namespace CityDefender
     class House : GameObject
     {
         private GamePanel _GamePanel;
-        public static bool r1count = false;
+        //Debug kode
+        //public static bool r1count = false;
         private int houseNr, totalHouseNr, etg;
-        Random r = new Random((int)DateTime.Now.Ticks);
+        /*
+         * Ikke vet jeg hvorfor, men random generatoren må tydligvis være static
+         * Mins 2 timer med irritasjon før jeg oppdaget det...
+         */
+        private static Random r = new Random((int)DateTime.Now.Ticks);
 
-        private int HOUSEHEIGHT = 10, HOUSEWIDTH = 34;
+        private int HOUSEHEIGHT = 10, HOUSEWIDTH = 34, HOUSESPACING = 5;
 
         public House(GamePanel _GamePanel, int houseNr, int totalHouseNr)
         {
             this._GamePanel = _GamePanel;
             this.houseNr = houseNr;
             this.totalHouseNr = totalHouseNr;
-            etg = 4 + r.Next(10);
+
+            //Minimum 2 etasjer + tilfeldig antall etg opp til 6
+            etg = 3 + r.Next(6);
 
             //Bredde 600, høyde 538, får ikke dritten til å funke så hardkoder det...
-            int houseSpaceing = 5;
-            xCoord = (538 / 2) - ((houseSpaceing + HOUSEWIDTH) * totalHouseNr) / 2 + (houseNr * (HOUSEWIDTH + houseSpaceing));
-            yCoord = 600;
-
-
-            // This is some broken C# shit
-            //xCoord = (_GamePanel.ClientRectangle.Size.Width / totalHouseNr) * (houseNr + 1);
-            //yCoord = _GamePanel.ClientRectangle.Size.Height;
+            //Dette er en formel for å sentrere byen. Den finner mitten av panelet, og mitten av byen, og starter med hus 1 helt til venstre.
+            xCoord = (600 / 2) - ((HOUSESPACING + HOUSEWIDTH) * totalHouseNr) / 2 + (houseNr * (HOUSEWIDTH + HOUSESPACING));
+            yCoord = 538;
         }
 
         public override void draw(Graphics g)
         {
-            Image newImage = global::CityDefender.Properties.Resources.etg; ;
-            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
-            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+            Image newImage = global::CityDefender.Properties.Resources.etg;
             for (int i = 0; i <= etg; i++)
             {
                 g.DrawImage(newImage, (int)xCoord, (int)yCoord - (HOUSEHEIGHT * (i + 1)), HOUSEWIDTH, HOUSEHEIGHT);
             }
-            if (!r1count)
+            
+            //Debug kode...
+            /*if (!r1count)
             {
-                g.DrawString("xCoord " + xCoord + " yCoord " + yCoord, new Font("Arial", 16), new SolidBrush(Color.Black), (int)100, (int)100);
+                g.DrawString("etg: " + etg + " yCoord " + yCoord, new Font("Arial", 16), new SolidBrush(Color.Black), (int)100, (int)100);
                 r1count = true;
-            }
-            //throw new NotImplementedException();
+            }*/
         }
     }
 }
